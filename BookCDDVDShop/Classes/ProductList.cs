ea578632a1987ba4c3094c455bed6e6c4c80feb5
AlexMD18 <alexmd2918@gmail.com>
2020-05-03 23:12:15 -0400

    Finished!

--- BookCDDVDShop/Classes/ProductList.cs
+++ BookCDDVDShop/Classes/ProductList.cs
@@ -2,9 +2,11 @@
  Alex Drogo / Sean Fuller
  Due Date: 05/04/2020
  CIS 3309_001
- Class Description: Product List - This class is used for creating and lists of recently accessed products. Used by the update method to 
- identify the current record to be updated.
+ Class Description: 
  */
+
+ //This class is used for creating and lists of recently accessed products
+ //Used by the update method to identify the current record to be updated
 
 using System;
 using System.Collections;
@@ -18,28 +20,23 @@ namespace BookCDDVDShop.Classes
     [Serializable()]
     public class ProductList
     {
-        //List of product objects
         private List<Product> hiddenProductList = new List<Product>();
 
-        //Parameterless Constructor 
         public ProductList()
         {
             hiddenProductList = new List<Product>();
         }
 
-        //Getter
         public Product getAnItem(int i)
         {
             return hiddenProductList[i];
         }
 
-        //Method to add object to the end of the list
         public void Add(Product p)
         {
             hiddenProductList.Add(p);
         }
 
-        //Gets the number of objects in the list
         public int Count()
         {
             return hiddenProductList.Count;
